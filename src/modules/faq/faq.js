f7e54cc6c4ca57221dcf12b5cd5653013bd3920f
Alex <astrodeep@ya.ru>
2023-04-05 08:02:46 +0400

    accordion vanila-js

--- src/modules/faq/faq.js
+++ src/modules/faq/faq.js
@@ -1,5 +1,5 @@
 //BEGIN
-$(".faq-article__appellation").on("click", function(e) {
+/*$(".faq-article__appellation").on("click", function(e) {
 
     e.preventDefault();
     let $this = $(this);
@@ -12,5 +12,39 @@ $(".faq-article__appellation").on("click", function(e) {
     $this.next().stop().slideToggle();
 
 
-});
+});*/
 //END
+
+
+// JS
+let acc = document.getElementsByClassName("faq-article__appellation");
+let i, clicked= false;
+
+let panel = acc[0].nextElementSibling;
+acc[0].classList.toggle("active");
+panel.style.maxHeight = panel.scrollHeight + "px";
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        clicked = true;
+
+        if(clicked){
+
+            for (k = 0; k < acc.length; k++) {
+                acc[k].classList.remove("active");
+                acc[k].nextElementSibling.style.maxHeight = null;
+            }
+            clicked = false;
+
+        }
+
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+
+}
